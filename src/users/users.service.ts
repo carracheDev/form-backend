9a2f558551db25d73110a11a76053bf8d8aaa44e
carracheDev <carrachesodjinou@gmail.com>
2026-02-17 01:15:50 +0100

    ajoute de sevices user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,38 +1,20 @@
 // src/users/users.service.ts
-import { ConflictException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
-import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async create(
-    email: string,
-    name: string,
-    password: string,
-    verifyToken: string,
-  ): Promise<User> {
-    try {
-      return await this.prisma.user.create({
-        data: {
-          email,
-          name,
-          password,
-          verifyToken: verifyToken || null,
-        },
-      });
-    } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === 'P2002') {
-          throw new ConflictException(
-            `Un utilisateur avec l'email ${email} existe déjà.`,
-          );
-        }
-      }
-      throw error;
-    }
+  async create(email: string, name: string, password: string): Promise<User> {
+    return this.prisma.user.create({
+      data: {
+        email,
+        name,
+        password,
+      },
+    });
   }
 
   async findByVerifyToken(token: string): Promise<User | null> {
